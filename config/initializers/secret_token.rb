229de8714b05a8927e24d9a0c9b1f531c2ce9b8e
Andreas Wagner <whysthatso@gmx.de>
2013-02-10 00:44:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hanna::Application.config.secret_token = 'f1e1ddc4f6dd2190b1bb465d0a4a3283549bea1b6a09bc0f269983d26c7f6d77de1875d4d76f43d125afbd36c4d350821a448b1e66975a6c175655715678d60d'
